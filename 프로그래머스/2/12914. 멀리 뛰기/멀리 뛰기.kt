101ca4a510520bdbbb22f33cc9a26d8e265359e3
unso99 <94777814+unso99@users.noreply.github.com>
2023-11-28 18:01:47 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.17 ms, Memory: 62 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun solution(n: Int): Long {
+        val dp = LongArray(n + 1)
+        
+        dp[0] = 1
+        dp[1] = 1
+        
+        for(i in 2..n){
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567
+        }
+        
+        return dp[n]
+    }
+}
